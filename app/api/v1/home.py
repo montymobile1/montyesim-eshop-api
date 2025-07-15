75e467b1b2f2ae7299dc5c504841a013ecf344db
Samah Jamal <samah.jamal@montymobile.com>
2025-07-15 10:02:14 +0300

    adding caching for home api

--- app/api/v1/home.py
+++ app/api/v1/home.py
@@ -1,4 +1,5 @@
 import os
+from functools import cache
 
 from fastapi import APIRouter, Depends, Header
 
@@ -12,6 +13,7 @@ router = APIRouter()
 service = HomeService()
 
 
+@cache
 @router.get("/", response_model=Response[HomeResponseDto], dependencies=[Depends(device_token)])
 async def home(x_currency: str = Header(os.getenv("DEFAULT_CURRENCY")), accept_language: str = Header("en"), ) -> \
         Response[HomeResponseDto]:
