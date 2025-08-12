2768086353b0727613fd6af438cd91e94d50463b
Samah Jamal <samah.jamal@montymobile.com>
2025-08-12 16:00:13 +0300

    update voucher redemption amount in currency

--- app/api/v1/voucher.py
+++ app/api/v1/voucher.py
@@ -1,19 +1,21 @@
 import os
 from typing import Annotated
 
+from fastapi import APIRouter, Depends, Header
+
 from app.dependencies.security import bearer_token, device_token
 from app.models.user import UserModel
 from app.schemas.response import Response
 from app.schemas.voucher import VoucherRequestRedeem
 from app.services.voucher_service import VoucherService
-from fastapi import APIRouter, Depends, Header
 
 router = APIRouter()
 
 service = VoucherService()
 
+
 @router.post("/redeem", response_model=Response,
              dependencies=[Depends(bearer_token), Depends(device_token)])
 async def assign(voucher_redeem_request: VoucherRequestRedeem, user: Annotated[UserModel, Depends(bearer_token)],
-                 x_device_id: str = Header(None),x_currency: str = Header(os.getenv("DEFAULT_CURRENCY"))):
-    return await service.redeem(voucher_redeem_request= voucher_redeem_request,user= user)
+                 x_device_id: str = Header(None), x_currency: str = Header(os.getenv("DEFAULT_CURRENCY"))):
+    return await service.redeem(voucher_redeem_request=voucher_redeem_request, user=user, x_currency=x_currency)
--- app/services/voucher_service.py
+++ app/services/voucher_service.py
@@ -1,10 +1,12 @@
+from loguru import logger
+
 from app.exceptions import CustomException
 from app.models.user import UserModel
 from app.repo.voucher_repo import VoucherRepo
 from app.schemas.response import ResponseHelper
 from app.schemas.voucher import VoucherRequestRedeem
+from app.services.currency_service import CurrencyService
 from app.services.user_wallet_service import UserWalletService
-from loguru import logger
 
 
 class VoucherService:
@@ -12,10 +14,11 @@ class VoucherService:
     def __init__(self):
         self.__voucher_repo = VoucherRepo()
         self.__user_wallet_service = UserWalletService()
+        self.__currency_service = CurrencyService()
 
-
-    async def redeem(self,voucher_redeem_request : VoucherRequestRedeem,user: UserModel):
-        voucher = self.__voucher_repo.get_first_by(where={"code" : voucher_redeem_request.code, "is_active" : True , "is_used" : False})
+    async def redeem(self, voucher_redeem_request: VoucherRequestRedeem, user: UserModel, x_currency: str):
+        voucher = self.__voucher_repo.get_first_by(
+            where={"code": voucher_redeem_request.code, "is_active": True, "is_used": False})
         if not voucher:
             raise CustomException(code=404, name="Voucher Redeem",
                                   details="Voucher Code Invalid")
@@ -34,8 +37,9 @@ class VoucherService:
                 raise CustomException(code=400, name="Voucher Redeem",
                                       details="Invalid voucher expiration date format")
         try:
-            await self.__user_wallet_service.add_wallet_transaction(voucher.amount,user.id,"voucher")
-            self.__voucher_repo.update_by(where={"id" : voucher.id},data={"used_by":user.id,"is_used" : True})
+            rate = self.__currency_service.get_rate_by_currency(x_currency)
+            await self.__user_wallet_service.add_wallet_transaction((voucher.amount * rate), user.id, "voucher")
+            self.__voucher_repo.update_by(where={"id": voucher.id}, data={"used_by": user.id, "is_used": True})
             return ResponseHelper.success_response()
         except Exception as ex:
             logger.error(str(ex))
