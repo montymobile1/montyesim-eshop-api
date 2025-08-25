3820b8ba4554bf582b981de45a87c09834c8b315
Samah Jamal <samah.jamal@montymobile.com>
2025-08-25 10:18:16 +0300

    handle referral code validation

--- app/services/voucher_service.py
+++ app/services/voucher_service.py
@@ -17,7 +17,7 @@ class VoucherService:
         self.__currency_service = CurrencyService()
 
     async def redeem(self, voucher_redeem_request: VoucherRequestRedeem, user: UserModel, x_currency: str):
-        is_used = self.__voucher_repo.get_first_by(where={"user_id": user.id,"is_used": True})
+        is_used = self.__voucher_repo.get_first_by(where={"is_used": True})
         if is_used:
             raise CustomException(code=400, name="Voucher Already Used",
                                   details="Voucher Already Used")
