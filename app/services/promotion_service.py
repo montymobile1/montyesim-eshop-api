391d7d5094e44575dbdf5989ac49d04e105a7aa9
Samah Jamal <samah.jamal@montymobile.com>
2025-09-29 10:10:04 +0300

    Refactor payment handling to include rule_id in wallet and DCB payment methods

--- app/services/promotion_service.py
+++ app/services/promotion_service.py
@@ -428,7 +428,7 @@ class PromotionService:
 
         if status != "completed":
             self.__promotion_usage_repo.update_by(where=condition, data={"status": status})
-            return
+            return None
         promotion_rule: PromotionRuleModel = self.__promotion_rule_repo.get_first_by(where={"id": rule_id})
 
         if is_referral:
@@ -439,7 +439,7 @@ class PromotionService:
             if referred_promotion_usage is None and referrer_promotion_usage is None:
                 logger.error(f"No pending promotion found for user {user_id} with code {code}")
                 self.__promotion_usage_repo.update_by(where=condition, data={"status": "failed"})
-                return
+                return None
             self.__promotion_usage_repo.update_by(where=condition, data={"status": status})
             return await self.__apply_referral_rewards(user_id=user_id, referral_code=code, paid_amount=paid_amount,
                                                        promotion_rule=promotion_rule)
