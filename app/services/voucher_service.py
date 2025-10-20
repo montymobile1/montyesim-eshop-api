487683c693390a5db6c512e56ff591050bbaa2b4
Samah Jamal <samah.jamal@montymobile.com>
2025-10-20 15:41:14 +0300

    Fix currency rate calculation in voucher service to ensure accurate wallet transaction amounts

--- app/services/voucher_service.py
+++ app/services/voucher_service.py
@@ -1,5 +1,3 @@
-import os
-
 from loguru import logger
 
 from app.config.constants import UserWalletTransactionSource, ErrorMessages
@@ -42,9 +40,9 @@ class VoucherService:
                                       details="Voucher Expired")
 
         try:
-            rate = self.__currency_service.get_currency_rate(from_currency="USD",
-                                                             to_currency=os.getenv("DEFAULT_CURRENCY", "USD"))
-            await self.__user_wallet_service.add_wallet_transaction(amount=(voucher.amount * rate), user_id=user.id,
+            rate = self.__currency_service.get_currency_rate(from_currency="USD", to_currency=x_currency)
+            await self.__user_wallet_service.add_wallet_transaction(amount=((int(voucher.amount * rate) * 100) / 100),
+                                                                    user_id=user.id,
                                                                     source=UserWalletTransactionSource.VOUCHER)
             self.__voucher_repo.update_by(where={"id": voucher.id}, data={"used_by": user.id, "is_used": True})
             return ResponseHelper.success_response()
