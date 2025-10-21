32be971277bbff8313f87292ec460a5e9e04b28a
Samah Jamal <samah.jamal@montymobile.com>
2025-10-21 13:05:21 +0300

    Refactor voucher redemption to remove unnecessary async calls and simplify currency handling

--- app/services/voucher_service.py
+++ app/services/voucher_service.py
@@ -7,7 +7,6 @@ from app.models.voucher import VoucherModel
 from app.repo.voucher_repo import VoucherRepo
 from app.schemas.response import ResponseHelper
 from app.schemas.voucher import VoucherRequestRedeem
-from app.services.currency_service import CurrencyService
 from app.services.user_wallet_service import UserWalletService
 
 
@@ -16,9 +15,8 @@ class VoucherService:
     def __init__(self):
         self.__voucher_repo = VoucherRepo()
         self.__user_wallet_service = UserWalletService()
-        self.__currency_service = CurrencyService()
 
-    async def redeem(self, voucher_redeem_request: VoucherRequestRedeem, user: UserModel, x_currency: str):
+    def redeem(self, voucher_redeem_request: VoucherRequestRedeem, user: UserModel, x_currency: str):
         is_used = self.__voucher_repo.get_first_by(where={"is_used": True, "code": voucher_redeem_request.code})
         if is_used:
             raise CustomException(code=400, name=ErrorMessages.VOUCHER_ALREADY_USED,
@@ -40,10 +38,9 @@ class VoucherService:
                                       details="Voucher Expired")
 
         try:
-            rate = self.__currency_service.get_currency_rate(from_currency="USD", to_currency=x_currency)
-            self.__user_wallet_service.add_wallet_transaction(amount=((int(voucher.amount * rate) * 100) / 100),
-                                                                    user_id=user.id,
-                                                                    source=UserWalletTransactionSource.VOUCHER)
+            self.__user_wallet_service.add_wallet_transaction(amount=voucher.amount,
+                                                              user_id=user.id,
+                                                              source=UserWalletTransactionSource.VOUCHER)
             self.__voucher_repo.update_by(where={"id": voucher.id}, data={"used_by": user.id, "is_used": True})
             return ResponseHelper.success_response()
         except Exception as ex:
