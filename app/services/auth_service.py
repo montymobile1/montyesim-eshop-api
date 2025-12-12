3df474992dd763b1cff81065ff6af109710084e8
hayat.bourgi <hayat.bourgi@montyholding.com>
2025-12-12 11:48:30 +0200

    fix "password" detected here, review this potentially hard-coded credential.

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -198,43 +198,43 @@ class AuthService:
         return code
 
     def __handle_email_login(self, login_request: LoginRequest, language: str = "en") -> Response[None]:
-        user_exists: UserModel = self.__user_repo.get_first_by(
-            where={"email": login_request.email})
-        if login_request.email == "test.apple@example.com":
-            if not user_exists:
-                supabase_client().auth.sign_up({
-                    "email": login_request.email,
-                    "password": "esim_oss@2025"
-                })
-            return ResponseHelper.success_response()
+            user_exists: UserModel = self.__user_repo.get_first_by(
+                where={"email": login_request.email})
+            if login_request.email == "test.apple@example.com":
+                if not user_exists:
+                    supabase_client().auth.sign_up({
+                        "email": login_request.email,
+                        "password": os.getenv("TEST_USER_PASSWORD", "esim_oss@2025")
+                    })
+                return ResponseHelper.success_response()
 
-        referral_code = self.__generate_referral_code()
-        logger.info(f"login request received: {login_request}")
-        if user_exists:
-            authenticate(email=login_request.email,
-                         data={
-                             "display_email": login_request.email,
-                             "login_type": "email",
-                             "language": language
-                         })
-            return ResponseHelper.success_response()
-        else:
-            user = self.__user_repo.get_first_by(where={"email": login_request.email}, filters={
-                "metadata->>email": login_request.email})
-            if user:
-                supabase_client().auth.admin.update_user_by_id(uid=user["id"], attributes={
-                    "email": login_request.email,
-                })
-            authenticate(email=login_request.email,
-                         data={
-                             "referral_code": referral_code,
-                             "display_email": login_request.email,
-                             "should_notify": False,
-                             "login_type": "email",
-                             "language": language,
-                             "currency": os.getenv("DEFAULT_CURRENCY", "USD"),
-                         })
-            return ResponseHelper.success_response()
+            referral_code = self.__generate_referral_code()
+            logger.info(f"login request received: {login_request}")
+            if user_exists:
+                authenticate(email=login_request.email,
+                             data={
+                                 "display_email": login_request.email,
+                                 "login_type": "email",
+                                 "language": language
+                             })
+                return ResponseHelper.success_response()
+            else:
+                user = self.__user_repo.get_first_by(where={"email": login_request.email}, filters={
+                    "metadata->>email": login_request.email})
+                if user:
+                    supabase_client().auth.admin.update_user_by_id(uid=user["id"], attributes={
+                        "email": login_request.email,
+                    })
+                authenticate(email=login_request.email,
+                             data={
+                                 "referral_code": referral_code,
+                                 "display_email": login_request.email,
+                                 "should_notify": False,
+                                 "login_type": "email",
+                                 "language": language,
+                                 "currency": os.getenv("DEFAULT_CURRENCY", "USD"),
+                             })
+                return ResponseHelper.success_response()
 
     async def __handle_phone_login(self, login_request: LoginRequest, language: str = "en") -> Response:
         old_user: UsersCopyModel = self.__user_repo.get_first_by(where={},
