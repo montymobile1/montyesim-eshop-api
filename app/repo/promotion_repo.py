0c3ee879147734e2da66bec72d0bc2ef3c20a0e3
Mohammad Tokko <mohammad.tokko@montymobile.com>
2025-12-23 14:49:40 +0200

    feat(promotion_service): implement select and update function for promotion usage

--- app/repo/promotion_repo.py
+++ app/repo/promotion_repo.py
@@ -17,3 +17,9 @@ class PromotionRepo(BaseRepository):
 class PromotionUsageRepo(BaseRepository):
     def __init__(self):
         super().__init__(DatabaseTables.TABLE_PROMOTION_USAGE, PromotionUsageModel)
+
+    # MT OSTE-932 select and update function
+    def select_and_update_usage(self, user_id: str, promotion_code: str, status: str):
+        return super().select_procedure(
+            where={"p_user_id": user_id, "p_promotion_code": promotion_code, "p_status": status},
+            function_name="promotion_usage_select_and_update")
